92c1c4f93bbf58fe1fe155fd4a94168145d553ae
yilmaz8596 <yilmaz8596@gmail.com>
2024-12-13 23:09:33 +0300

    fix deployment bug

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://icychat.onrender.com",
     credentials: true,
   })
 );
