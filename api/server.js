236a9c2e8291d1916edc5ced61460945e75923c7
yilmaz8596 <yilmaz8596@gmail.com>
2024-12-13 21:46:28 +0300

    prepare for deployment

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import path from "path";
 import authRouter from "./routes/auth.routes.js";
 import messageRouter from "./routes/message.routes.js";
 import userRouter from "./routes/user.routes.js";
@@ -14,6 +14,8 @@ import { connectToDb } from "./db/connectToDb.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json());
@@ -29,6 +31,12 @@ app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
 app.use("/api/user", userRouter);
 app.use("/api/conversation", conversationRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   err.status = err.status || 500;
